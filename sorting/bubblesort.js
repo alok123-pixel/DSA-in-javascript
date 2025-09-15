84e236251887a8b213150ae46e232f0f44b274d8
Alok123-pixel <mshlok75@gmail.com>
2025-09-15 21:57:44 +0530

    sorting question

---
+++ sorting/bubblesort.js
@@ -0,0 +1,22 @@
+// In this we will have a arr and we have to sort it in assending order using bubble sort
+// so basically in bubble sort we will comapre the adjacent elements and if the first element is greater than its next element we perform a swapping
+// lets see how to do it
+
+
+function bubblesort(arr){
+    let n = arr.length;;
+    for(let i = 0; i < n-1; i++){
+        let swap = false;
+        for(let j = 0; j < n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+                swap = true;
+            }
+        } if(swap === false){
+            break;
+        }
+    }
+    return arr;
+}
+
+console.log(bubblesort([12,99,34,11,99,56,1,2,4,8,10,67,15,45,93,45,67,98]));
