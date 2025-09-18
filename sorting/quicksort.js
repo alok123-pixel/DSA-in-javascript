226e544b8d9f7551b886e843a3c2a550eea2f257
Alok123-pixel <mshlok75@gmail.com>
2025-09-18 22:15:36 +0530

    quick-sort

---
+++ sorting/quicksort.js
@@ -0,0 +1,19 @@
+// quicksort algorithm
+
+function quicksort(arr){
+    if(arr.length <= 1){
+        return arr;
+    }
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] < pivot){
+            left.push(arr[i]);
+        }else{
+            right.push(arr[i]);
+        }
+    }
+    return [...quicksort(left),pivot,...quicksort(right)];
+}
+console.log(quicksort([5,7,3,6,1,4,9,8]));
