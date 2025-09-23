6ce960fe5d9dd3e475edbf91a5c81b77f9ee7e89
Alok123-pixel <mshlok75@gmail.com>
2025-09-23 22:31:47 +0530

    two-pointer

---
+++ two-pointer/moveZeroesend.js
@@ -0,0 +1,15 @@
+// moving zeroes to the end
+
+function moveZeroes(arr){
+    let j = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] != 0){
+            let hold = arr[j];
+            arr[j] = arr[i];
+            arr[i] = hold;
+            j++;
+        }
+    }
+    return arr;
+}
+console.log(moveZeroes([1,3,4,0,7,0]));
