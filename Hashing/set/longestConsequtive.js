dfebefd0cd85a7d859bb741c0f79c19c52379345
Alok123-pixel <mshlok75@gmail.com>
2026-09-18 16:27:54 +0530

    Longest consequtive solution

---
+++ Hashing/set/longestConsequtive.js
@@ -0,0 +1,55 @@
+// 128. Longest Consecutive Sequence
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// You must write an algorithm that runs in O(n) time.
+
+ 
+
+// Example 1:
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+// Example 2:
+
+// Input: nums = [0,3,7,2,5,8,4,6,0,1]
+// Output: 9
+// Example 3:
+
+// Input: nums = [1,0,1,2]
+// Output: 3
+ 
+
+// Constraints:
+
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+
+function ConsequtiveSequence(nums){
+    let set = new Set();
+    for(let i=0; i< nums.length; i++){
+        set.add(nums[i]);
+    }
+
+    let longest = 0;
+    for(let a of set){
+        if(!set.has(a - 1)){
+            let currentSum = a;
+            let length = 1;
+
+            while(set.has(currentSum + 1)){
+                currentSum++;
+                length++;
+            }
+            longest = Math.max(longest,length)
+        }
+    }
+    return longest;
+}
+
+console.log(ConsequtiveSequence([100,4,200,1,3,2]));
