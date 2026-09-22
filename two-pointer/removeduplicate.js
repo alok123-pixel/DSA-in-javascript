eacb7d6cc048b0b4ee59dc19a2223ed78a785725
Alok123-pixel <mshlok75@gmail.com>
2026-09-22 15:40:30 +0530

    remove duplicate

--- two-pointer/removeduplicate.js
+++ two-pointer/removeduplicate.js
@@ -1,16 +1,28 @@
 // Remove duplicates return the unique element
 
-function removeDuplicates(arr){
-  if(arr.length === 0){
-    return 0;
-  }
-  j = 0;
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] != arr[i+1]){
-      arr[j] = arr[i];
-      j++;
+// function removeDuplicates(arr){
+//   if(arr.length === 0){
+//     return 0;
+//   }
+//   j = 0;
+//   for(let i = 0; i < arr.length; i++){
+//     if(arr[i] != arr[i+1]){
+//       arr[j] = arr[i];
+//       j++;
+//     }
+//   }
+//   return j;
+// }
+// console.log(removeDuplicates([1,1,1,2,2,3,3,4]));
+// If someone says that show me array after removing the duplicate
+function removeDuplicate(arr){
+    let i = 0;
+    for(let j =1; j<arr.length; j++){
+        if(arr[j] !== arr[i]){
+            i = i+1;
+            arr[i] = arr[j];
+        }
     }
-  }
-  return j;
+    return arr.slice(0,i+1);
 }
-console.log(removeDuplicates([1,1,1,2,2,3,3,4]));
+console.log(removeDuplicate([1,1,1,2,2,3,3,4]));
